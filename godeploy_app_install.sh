9e637e4dd54387bf968266625df2af9f56051ff3
archit2693 <archit2693.aj@gmail.com>
2018-07-06 13:33:11 +0530

    update readme

--- godeploy_app_install.sh
+++ godeploy_app_install.sh
@@ -1,3 +1,3 @@
 curl -L https://omnitruck.chef.io/install.sh | sudo bash
-sudo curl -L https://raw.githubusercontent.com/archit2693/godeploy-install/master/nodes/godeploy_app_node.json > /home/godeploy_app_node.json
+sudo curl -L https://raw.githubusercontent.com/archit2693/godeploy-install/master/nodes/godeploy_app_node.json.example > /home/godeploy_app_node.json
 sudo chef-solo -j /home/godeploy_app_node.json --recipe-url https://github.com/archit2693/godeploy-install/raw/master/chef-solo.tar.gz
--- postgres_install.sh
+++ postgres_install.sh
@@ -1,3 +1,3 @@
 curl -L https://omnitruck.chef.io/install.sh | sudo bash
-sudo curl -L https://raw.githubusercontent.com/archit2693/godeploy-install/master/nodes/postgres_server_node.json > /home/postgres_server_node.json
+sudo curl -L https://raw.githubusercontent.com/archit2693/godeploy-install/master/nodes/postgres_server_node.json.example > /home/postgres_server_node.json
 sudo chef-solo -j /home/postgres_server_node.json --recipe-url https://github.com/archit2693/godeploy-install/raw/master/chef-solo.tar.gz
