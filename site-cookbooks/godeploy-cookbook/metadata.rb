5644d0eb5a7f99edef937ffdceb6aa41a392df83
archit2693 <archit2693.aj@gmail.com>
2018-07-11 11:49:39 +0530

    add binding host for app service

--- site-cookbooks/godeploy-cookbook/metadata.rb
+++ site-cookbooks/godeploy-cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'archit.j.aux@go-jek.com'
 license 'All Rights Reserved'
 description 'Installs/Configures godeploy'
 long_description 'Installs/Configures godeploy'
-version '0.3.16'
+version '0.3.17'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 depends 'tar'
